fa02ea9df351de6fd562aebbb436e79da808039a
Daniyar <dkurbanov7639@gmail.com>
2022-11-30 21:22:01 +0500

    init:razbivka na komponenti

---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+const Profile = () => {
+    return <main className="main">
+    <div className="main-img1">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCZlf5lc5tX-0gY-y94pGS0mQdL-D0lCH2OQ&usqp=CAU"></img>
+    </div>
+    <div>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjF4BI5W-dkxj-FxchHNeTvObAykcJsWofWk4KTL4z5nnFNKf0By0lW1GpvbckD2iUYmE&usqp=CAU"></img>
+    </div>
+    <div>
+      My post
+      <div>New post</div>
+      <div>
+        <div>Post1</div>
+        <div>Post2</div>
+      </div>
+    </div>
+  </main>
+}
+
+export default Profile;
