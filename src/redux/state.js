cd7ff572e8110b970744cb4ca61adf917f05be05
Daniyar <dkurbanov7639@gmail.com>
2022-12-09 11:33:27 +0500

    modified last commit

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,34 +1,29 @@
-
-
-
 let state = {
+  dialogPage: {
+    dialogs: [
+      { id: 1, name: "Dimych" },
+      { id: 2, name: "Andrey" },
+      { id: 3, name: "Sveta" },
+      { id: 4, name: "Sasha" },
+      { id: 5, name: "Valera" },
+    ],
+    messages: [
+      { id: 1, message: "Hello" },
+      { id: 2, message: "Hi how are you?" },
+      { id: 3, message: "Yo" },
+      { id: 4, message: "yo" },
+      { id: 5, message: "Hi" },
+    ],
+  },
 
-    dialogPage: {
-        dialogs: [
-            {id: 1, name: 'Dimych'},
-            {id: 2, name: 'Andrey'},
-            {id: 3, name: 'Sveta'},
-            {id: 4, name: 'Sasha'},
-            {id: 5, name: 'Valera'},
-          ],          
-        messages: [
-            {id: 1, message: 'Hello'},
-            {id: 2, message: 'Hi how are you?'},
-            {id: 3, message: 'Yo'},
-            {id: 4, message: 'yo'},
-            {id: 5, message: 'Hi'},
-          ]
-    },
-    profilePage: {
-         posts: [
-            {id: 1, message: "Hi, How are you Mr Poster?", likesCount: 12},
-            {id: 2, message: "It's my first post", likesCount: 1},
-            {id: 3, message: "It's my second post", likesCount: 5},
-            {id: 4, message: "It's my 3rd post", likesCount: 9},
-          ]        
-        }
-
-
+  profilePage: {
+    posts: [
+      { id: 1, message: "Hi, How are you Mr Poster?", likesCount: 12 },
+      { id: 2, message: "It's my first post", likesCount: 1 },
+      { id: 3, message: "It's my second post", likesCount: 5 },
+      { id: 4, message: "It's my 3rd post", likesCount: 9 },
+    ],
+  },
 };
 
-  export default state;
+export default state;
