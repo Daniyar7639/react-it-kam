9900baad32dc75d794d758c7ab351219a591bc88
Daniyar <dkurbanov7639@gmail.com>
2022-12-24 23:29:46 +0500

    Pagination lesson 55

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,22 +2,58 @@ import axios from "axios";
 import React from "react";
 import styles from './Users.module.css'
 import userPhoto from '../../assets/images/user.png'
-import News from "../News/News";
+
 
 
 
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
-    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {   
+    /*constructor(props) {
+        super(props);  
+ };                              konstruktor vizivaetsya po umolchaniyu*/
+    componentDidMount () {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize} `).then(response => {   
     this.props.setUsers(response.data.items);
+    this.props.setTotalUsersCount(response.data.totalCount)
   }); 
- };
+    }
 
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize} `).then(response => {   
+            this.props.setUsers(response.data.items);
+          });
+    }
+
+ 
   render = () => {
+
+    let pagesCount = Math.ceil(this.props.totalUsersCount/this.props.pageSize);
+    let pages = [];
+    for (let i=1; i<=pagesCount; i++) {
+        pages.push(i);
+    }
+    //Карусель массива номеров страниц, можно сделать через Array.slice():
+    let curP = this.props.currentPage;
+    let curPF = ((curP - 5) < 0) ?  0  : curP - 5 ;
+    let curPL = curP + 5;
+    let slicedPages = pages.slice( curPF, curPL); 
+    /* И в jsx меняем:
+    pages.map(*****) 
+     на 
+    slicedPages.map(*****)*/
+
     return <div>
+        <div>
+            {slicedPages.map(p =>  {
+                return <spam className={this.props.currentPage === p && styles.selectedPage} 
+                onClick={(e) => {this.onPageChanged(p)}} >{p}</spam>
+
+            })}
+            
+
+        </div>
         {
             this.props.users.map (u => <div key={u.id}>
                 <div>
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,10 +1,13 @@
 import { connect } from "react-redux";
-import { followAC, setUsersAC, unfollowAC } from "../../redux/Users-reduser";
+import { followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC } from "../../redux/Users-reduser";
 import Users from "./Users";
 
 const mapStoreToProps = (state) => {
     return {
-        users:state.usersPage.users
+        users:state.usersPage.users,
+        pageSize:state.usersPage.pageSize,
+        totalUsersCount:state.usersPage.totalUsersCount,
+        currentPage:state.usersPage.currentPage
     }
 }
 const mapDispatchToProps = (dispatch) => {
@@ -17,6 +20,12 @@ const mapDispatchToProps = (dispatch) => {
         },
         setUsers: (users) => {
             dispatch(setUsersAC(users));
+        },
+        setCurrentPage: (pageNumber) => {
+            dispatch(setCurrentPageAC(pageNumber));
+        },
+        setTotalUsersCount: (totalCount) => {
+            dispatch(setTotalUsersCountAC(totalCount));
         }
     }
 }
